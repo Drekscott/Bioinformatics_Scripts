d869fbd7982e7fa0ac0c3c1f21dca192dcd6896c
Dreyton Scott <dreytonscott@Dreytons-MacBook-Pro.local>
2017-04-02 22:04:08 -0400

    improved algorithm, eliminated multiple lines of code

--- findpattern.py
+++ findpattern.py
@@ -17,20 +17,13 @@ class FindPatternLocation:
         self.oricpattern = pattern
         self.filetoread = file
         self.find_nucleotide_pattern_occurence()
-        
+
     def find_nucleotide_pattern_occurence(self):
-        count = 0
-        string_count = 0
         self.filetoread = open(self.filetoread,"r")
-        for line in self.filetoread:
-            for word in line:
-                string_count +=1
-            for i in range(string_count):
-                pattern_to_check = line[i:i+len(self.oricpattern)]
-                if pattern_to_check == self.oricpattern:
-                    count += 1
-        print ("Nucleotide Count: %d") % count
-                
+        data_file = self.filetoread.read()
+        self.filetoread.close()
+        print ("Nucleotide Count: %d") % data_file.count(self.oricpattern)
+
 if (len(sys.argv) != 3):
     print "Usage: %s <pattern> <file>" % sys.argv[0]
 else:
